c22fd9471df62c37a87134077d7e09971134408d
vedrano7 <vedran.delic7@gmail.com>
2023-10-11 20:03:53 +0200

    kraj labova1

--- lab1/lab1/lab1.c
+++ lab1/lab1/lab1.c
@@ -1,7 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+#define MAX 100
 
-void main() {
 
-	printf("hello world");
 
+typedef struct {
+	char name[MAX];
+	char surname[MAX];
+	double bodovi;
+} student;
+
+int readNoRows() {
+	int counter = 0;
+	FILE* datoteka = NULL;
+	char buffer[MAX] = { 0 };
+
+	datoteka = fopen("studenti.txt", "r");
+
+	if (!datoteka) {
+		printf("File not open");
+		return -1;
+	}
+
+	while (!feof(datoteka)) {
+		fgets(buffer, MAX, datoteka);
+		counter++;
+	}
+
+
+	fclose(datoteka);
+
+	return counter;
+};
+
+
+
+
+int main() {
+
+	int brred;
+
+	brred = readNoRows;
+
+	printf("%d", readNoRows);
+
+	return 0;
 }
