4f25d18f999983e34c2746f3e42acd0f414360bc
vedrano7 <vedran.delic7@gmail.com>
2023-12-07 00:41:48 +0100

    rename

---
+++ lab/lab2,3/lab2,3.c
@@ -0,0 +1,388 @@
+﻿/*2. Definirati strukturu osoba(ime, prezime, godina rođenja) i napisati program koji :
+A.dinamički dodaje novi element na početak liste,
+B.ispisuje listu,
+C.dinamički dodaje novi element na kraj liste,
+D.pronalazi element u listi(po prezimenu),
+E.briše određeni element iz liste,
+U zadatku se ne smiju koristiti globalne varijable.*/
+
+/*3. Prethodnom zadatku dodati funkcije:
+A. dinamički dodaje novi element iza određenog elementa,
+B. dinamički dodaje novi element ispred određenog elementa,
+C. sortira listu po prezimenima osoba (!!NAPRAVIT SORTIRANI UNOS!!),
+D. upisuje listu u datoteku,
+E. čita listu iz datoteke.*/
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+
+typedef struct person* position;
+
+typedef struct person {
+	char name[MAX];
+	char surname[MAX];
+	int birthYear;
+
+	position next;
+}
+Person;
+
+int menu(position head);
+int addPersonToBeg(position P);
+int printList(position P);
+int addPersonToEnd(position P);
+position findPerson(position P);
+int del(position P);
+int insertAfterPerson(position P);
+int insertBeforePerson(position P);
+int sortedInput(position P);
+int inputListToFIle(position P);
+int inputListFromFile(position P);
+
+int main() {
+	Person head = { {0},{0},0,NULL};
+
+	menu(&head);
+
+	return 0;
+}
+
+int menu(position head) {
+	char pick='\0';
+	position resPerson = NULL;
+
+		while (1) {
+			printf("\nZa dodati novi element na pocetak liste upisite 1, za ispisati listu upisite 2, za dodati novi element na kraj liste upisite 3, za pronaci odredeni element u listi po prezimenu upisite 4, za izbrisati odredeni element iz liste upisite 5, za umetnuti novi element iza odredenog elementa upisite 6, za umetnuti novi element ispred odredenog elementa upisite 7, za sortirani unos (od manjeg prema vecem) upisite 8, za upisati listu u datoteku upisite 9, za ispisati listu iz datoteke upisite 'a' te upisite 0 za izaci iz programa\n");
+			scanf("\n%c", &pick);
+			switch (pick){
+			
+				case '1':
+
+					addPersonToBeg(head);
+
+					continue;
+
+				case '2':
+
+					printList(head->next);
+
+					continue;
+
+				case '3':
+
+					addPersonToEnd(head);
+
+					continue;
+
+				case '4':
+					resPerson=findPerson(head->next);
+					if (resPerson == NULL)
+						printf("\nOsoba nije nadena u listi\n");
+					else
+						printf("\nOsoba po prezimenu %s je nadena na adresi %p\n", resPerson,resPerson->surname);;
+
+					continue;
+
+				case '5':
+					del(head);
+
+					continue;
+
+				case '6':
+					insertAfterPerson(head->next);
+
+					continue;
+
+				case '7':
+					insertBeforePerson(head);
+
+					continue;
+
+				case '8':
+					sortedInput(head);
+
+					continue;
+
+				case '9':
+					inputListToFIle(head->next);
+
+					continue;
+
+				case 'a':
+					inputListFromFile(head);
+
+					continue;
+
+				case '0':
+
+					printf("\nKraj programa!\n");
+
+					break;
+
+				default:
+					printf("\nKrivi unos!!\n");
+
+					continue;
+			}
+			
+			break;
+		};
+
+
+	return 0;
+}
+
+int addPersonToBeg(position P){
+	position newPerson=NULL;
+
+	newPerson =(Person*)malloc(sizeof(Person));
+
+	if (!newPerson) {
+		printf("\nNeuspjela alokacija memorije za newPerson\n");
+		exit(-1);
+	}
+
+	newPerson->next = NULL;
+
+	printf("\nUnesite ime, prezime i godinu rodenja nove osobe:\n");
+	scanf("\n%s %s %d", newPerson->name, newPerson->surname, &newPerson->birthYear);
+	
+	newPerson->next = P->next;
+	P->next = newPerson;
+
+	return 0;
+}
+
+int printList(position P) {
+
+	printf("\n------------\n"); 
+	
+	if (P == NULL)
+		printf("Lista je prazna!\n");
+
+	else{
+
+		while (P != NULL) {
+			printf("%s %s %d\n", P->name, P->surname, P->birthYear);
+			P = P->next;
+		}
+	}
+
+	printf("------------\n");
+
+	return 0;
+}
+
+int addPersonToEnd(position P) {
+
+	position newPerson=NULL;
+
+	newPerson =(Person*)malloc(sizeof(Person));
+
+	if (!newPerson) {
+		printf("\nNeuspjela alokacija memorije za newPerson\n");
+		exit(-1);
+	}
+
+	printf("\nUnesite ime, prezime i godinu rodenja nove osobe: \n");
+	scanf("\n%s %s %d", newPerson->name, newPerson->surname, &newPerson->birthYear);
+
+	while (P->next != NULL) {
+		P = P->next;
+	}
+
+	newPerson->next = P->next;
+	P->next = newPerson;
+
+	return 0;
+}
+
+position findPerson(position P){
+	
+	char wantedPerson[MAX];
+
+	printf("\nUnesite prezime trazene osobe: \n");
+	scanf("\n%s", wantedPerson);
+
+	while (P != NULL && strcmp(P->surname, wantedPerson)!=0) {
+		P = P->next;
+
+	}
+
+	return P;
+	}
+
+int del(position P) {
+	Person wantedPersonToDel = { {0},{0},0,NULL };
+	position temp=NULL;
+
+	printf("\nUnesite ime, prezime i god rodenja osobe koju zelite izbrisati iz liste:\n");
+	scanf("\n%s %s %d%", wantedPersonToDel.name, wantedPersonToDel.surname, &wantedPersonToDel.birthYear);
+
+
+	while (P->next != NULL && strcmp(P->next->surname, wantedPersonToDel.surname)!=0 && strcmp(P->next->name, wantedPersonToDel.name)!=0 && P->next->birthYear!=wantedPersonToDel.birthYear){
+		
+		P = P->next;
+	}
+
+
+	if (P->next == NULL)
+		printf("\nTe osoba nema na listi\n");
+
+	else {
+		temp = P->next;
+		P->next = temp->next;
+		printf("\nOsoba %s %s %d je obrisana!\n", wantedPersonToDel.name, wantedPersonToDel.surname, wantedPersonToDel.birthYear);
+	
+	}
+
+	free(temp);
+
+	return 0;
+}
+
+int insertAfterPerson(position P) {
+	position newPerson = NULL;
+	Person afterWhichPersonToInsert = { {0},{0},0 };
+
+	newPerson = malloc(sizeof(Person));
+
+	if (!newPerson) {
+		printf("\nNeuspjela alokacija memorije za newPerson\n");
+		exit(-1);
+	}
+
+	printf("\nUnesite ime, prezime i godinu rodenja osobe iza koje zelite umetnuti novu osobu: \n");
+	scanf("\n%s %s %d", afterWhichPersonToInsert.name, afterWhichPersonToInsert.surname, &afterWhichPersonToInsert.birthYear);
+
+	printf("\nUnesite ime, prezime i godinu rodenja nove osobe: \n");
+	scanf("\n%s %s %d", newPerson->name, newPerson->surname, &newPerson->birthYear);
+
+	while (P != NULL && strcmp(P->surname, afterWhichPersonToInsert.surname) != 0 && strcmp(P->name, afterWhichPersonToInsert.name) != 0 && P->birthYear != afterWhichPersonToInsert.birthYear) {
+		P = P->next;
+	}
+
+	if (P == NULL) {
+		printf("\nmOsoba iza koje ste htjeli umetnuti novu osobu ne postoji u listi\n");
+		free(newPerson);
+	}
+
+	else {
+		newPerson->next = P->next;
+		P->next = newPerson;
+	}
+
+	return 0;
+}
+
+int insertBeforePerson(position P) {
+	position newPerson = NULL;
+	Person persoInFront = { {0},{0},0 };
+
+	newPerson = malloc(sizeof(Person));
+
+	if (!newPerson) {
+		printf("\nNeuspjela alokacija memorije za newPerson\n");
+		exit(-1);
+	}
+
+	printf("\nUnesite ime, prezime i godinu rodenja osobe ispred koje zelite umetnuti novu osobu: \n");
+	scanf("\n%s %s %d", persoInFront.name, persoInFront.surname, &persoInFront.birthYear);
+
+	printf("\nUnesite ime, prezime i godinu rodenja nove osobe: \n");
+	scanf("\n%s %s %d", newPerson->name, newPerson->surname, &newPerson->birthYear);
+
+	while (P != NULL && strcmp(P->next->surname, persoInFront.surname) != 0 && strcmp(P->next->name, persoInFront.name) != 0 && P->next->birthYear != persoInFront.birthYear) {
+		P = P->next;
+
+	}
+
+	if (P == NULL) {
+		printf("\nmOsoba ispred koje ste htjeli umetnuti novu osobu ne postoji u listi\n");
+		free(newPerson);
+	}
+	
+	else {
+		newPerson->next = P->next;
+		P->next = newPerson;
+
+	}
+
+	return 0;
+}
+
+int sortedInput(position P) {
+	position newPerson = NULL;
+
+	newPerson = malloc(sizeof(Person));
+
+	if (!newPerson) {
+		printf("\nNeuspjela alokacija memorije za newPerson\n");
+		exit(-1);
+	}
+
+	printf("\nUnesite ime, prezime i godinu rodenja nove osobe: \n");
+	scanf("\n%s %s %d", newPerson->name, newPerson->surname, &newPerson->birthYear);
+
+	while (P->next!= NULL && strcmp(newPerson->surname, P->next->surname) > 0)
+		P = P->next;
+
+
+	newPerson->next = P->next;
+	P->next = newPerson;
+
+	return 0;
+}
+
+int inputListToFIle(position P) {
+	FILE* students = NULL;
+
+	students = fopen("studenti.txt", "w");
+
+	if (!students) {
+		printf("\nFile students unsuccesfully opened!\n");
+		return -1;
+	}
+
+	while (P != NULL) {
+		fprintf(students, "%s %s %d\n", P->name, P->surname, P->birthYear);
+		P = P->next;
+	}
+
+	fclose(students);
+
+	return 0;
+}
+
+int inputListFromFile(position P) {
+	FILE* students = NULL;
+	position newPerson=NULL;
+
+	students = fopen("studenti.txt", "r");
+
+	if (!students) {
+		printf("\nFile students unsuccesfully opened!\n");
+		return -1;
+	}
+
+	while (!feof(students)) {
+
+		newPerson = malloc(sizeof(Person));
+
+		fscanf(students, "%s %s %d\n", newPerson->name, newPerson->surname, &newPerson->birthYear);
+
+		newPerson->next = P->next;
+		P->next = newPerson;
+
+		P = P->next;
+		
+	}
+
+	fclose(students);
+
+	return 0;
+}
---
+++ lab/lab4/lab3/lab4.c
@@ -0,0 +1,322 @@
+﻿/*4. Napisati program za zbrajanje i množenje polinoma.Koeficijenti i eksponenti se
+čitaju iz datoteke.
+Napomena: Eksponenti u datoteci nisu nužno sortirani.*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define FILE_NOT_OPEN (-1)
+#define FAILED_MEMORY_ALLOCATION (NULL)
+#define MAX_LINE (1024)
+
+typedef struct element* position;
+typedef struct element {
+	int coeff;
+	int exp;
+	position next;
+}Element;
+
+int readPolyFromFile(position headPoly1, position headPoly2);
+int parseStringIntoList(position headPoly, char buffer[MAX_LINE]);
+position createElement(int coeff, int exp);
+int sortedInput(position headPoly, position newElement);
+int mergeElements(position prevElementInList, position newElement);
+int printPoly(char* polyName,position firstEl);
+int polySum(position firstElPoly1, position firstElPoly2, position headPolySum);
+int polyProduct(position firstElPoly1, position firstElPoly2, position headPolyProduct);
+
+int main() {
+	Element headPoly1 = { 0,0,NULL }, headPoly2 = { 0,0,NULL }, headPolySum = {0,0,NULL}, headPolyProduct = { 0,0,NULL };
+
+	if (readPolyFromFile(&headPoly1, &headPoly2) == EXIT_SUCCESS) {
+
+		printPoly("Prvi polinom: " ,headPoly1.next);
+		printPoly("Drugi polinom: ",headPoly2.next);
+	}
+
+	polySum(headPoly1.next, headPoly2.next, &headPolySum);
+
+	printPoly("Suma polinoma: ", headPolySum.next);
+
+	polyProduct(headPoly1.next, headPoly2.next, &headPolyProduct);
+
+	printPoly("Umnozak polinoma: ", headPolyProduct.next);
+
+	return EXIT_SUCCESS;
+}
+
+int readPolyFromFile(position headPoly1, position headPoly2) {
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+	int status = EXIT_SUCCESS;
+
+
+	filePointer = fopen("polinomi.txt", "r");
+
+	if (!filePointer) {
+		printf("\nCan't open file polinomi.txt!\n");
+		return FILE_NOT_OPEN;
+	}
+
+	fgets(buffer, MAX_LINE, filePointer);		//uzme prvi cili line (redak) teksta sta je zap cili prvi polinom
+
+	status = parseStringIntoList(headPoly1,buffer);
+
+	if (status != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+
+	fgets(buffer, MAX_LINE, filePointer);
+
+	status = parseStringIntoList(headPoly2, buffer);
+
+	if (status != EXIT_SUCCESS)
+		return EXIT_FAILURE;
+	
+	fclose(filePointer);
+
+	return EXIT_SUCCESS;
+}
+
+int parseStringIntoList(position headPoly, char buffer[MAX_LINE]) {
+	int coeff = 0, exp = 0,numBytes=0;
+	int status = EXIT_SUCCESS;
+	position newElement = NULL;
+
+	while (strlen(buffer)>0) {
+		status=sscanf(buffer, " %dx^%d %n", &coeff, &exp, &numBytes);
+
+		if (status != 2) {							//zato sto sscanf vraca broj skeniranih podataka, a u ovom slucaju bi to trebalo bit 2
+			printf("\nThis file is not good!\n");
+			return EXIT_FAILURE;
+		}
+			
+		newElement=createElement(coeff, exp);
+
+		if (!newElement)
+			return EXIT_FAILURE;
+
+		sortedInput(headPoly, newElement);
+
+
+		buffer += numBytes;
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
+position createElement(int coeff, int exp) {
+	position newElement = NULL;
+
+	newElement = malloc(sizeof(Element));
+
+	if (!newElement) {
+		printf("\nUnable to locate memmory for newElement!\n");
+		return FAILED_MEMORY_ALLOCATION;
+	}
+
+	newElement->coeff = coeff;
+	newElement->exp = exp;
+	newElement->next = NULL;
+
+	return newElement;
+}
+
+int sortedInput(position headPoly, position newElement) {
+	position current = headPoly;
+
+	while (current->next!=NULL && newElement->exp < current->next->exp)
+		current = current->next;
+
+	if (current->next==NULL || newElement->exp > current->next->exp){
+
+		newElement->next = current->next;
+		current->next = newElement;
+	}
+
+
+	else{
+		
+		mergeElements(current, newElement);
+	}
+			
+	return EXIT_SUCCESS;
+}
+
+int mergeElements(position prevElementInList, position newElement) {
+	int resultCoeff = 0;
+	position temp = NULL;
+
+	resultCoeff = prevElementInList->next->coeff + newElement->coeff;
+
+	if (resultCoeff != 0)
+		prevElementInList->next->coeff = resultCoeff;
+
+	else {
+		temp = prevElementInList->next;
+		prevElementInList->next = temp->next;
+
+		free(temp);
+	
+	}
+
+	free(newElement);		// ovo radimo jer ako mergeamo onda ne moramo dodavat novi element polinoma u listu nego samo nadodajemo na postojeci pa nam je newElement beskorisan
+
+
+	return EXIT_SUCCESS;
+}
+
+int polySum(position firstElPoly1, position firstElPoly2, position headPolySum) {
+	/*position currentPoly1 = firstElPoly1, currentPoly2 = firstElPoly2, newElement = NULL,temp=NULL;
+	int coeff = 0, exp = 0;
+
+
+	while (currentPoly1 != NULL && currentPoly2 != NULL) {
+
+		if (currentPoly1->exp == currentPoly2->exp) {
+			exp = currentPoly1->exp;
+			coeff = currentPoly1->coeff + currentPoly2->coeff;
+			
+			newElement = createElement(coeff, exp);
+			sortedInput(headPolySum, newElement);
+		}
+
+		else {
+			exp = currentPoly1->exp;
+			coeff = currentPoly1->coeff;
+
+			newElement = createElement(coeff, exp);
+			sortedInput(headPolySum, newElement);
+
+			exp = currentPoly2->exp;
+			coeff = currentPoly2->coeff;
+
+			newElement = createElement(coeff, exp);
+			sortedInput(headPolySum, newElement);
+		}
+
+		currentPoly1 = currentPoly1->next;
+		currentPoly2 = currentPoly2->next;
+
+	}
+
+	if (currentPoly1 == NULL)
+		temp = currentPoly2;
+
+	else
+		temp = currentPoly1;
+
+	while (temp != NULL) {
+
+		exp = temp->exp;
+		coeff = temp->coeff;
+
+		newElement = createElement(coeff, exp);
+		sortedInput(headPolySum, newElement);
+
+		temp = temp->next;
+	}*/
+
+	//vise programerski nacin za zbrojit polinome
+
+	position currentPoly1 = firstElPoly1, currentPoly2 = firstElPoly2, newElement = NULL;
+	int coeff = 0, exp = 0;
+
+	while (currentPoly1 != NULL) {
+		exp = currentPoly1->exp;
+		coeff = currentPoly1->coeff;
+
+		newElement = createElement(coeff, exp);
+		sortedInput(headPolySum, newElement);
+
+		currentPoly1 = currentPoly1->next;
+	}
+
+	while (currentPoly2 != NULL) {
+		exp = currentPoly2->exp;
+		coeff = currentPoly2->coeff;
+
+		newElement = createElement(coeff, exp);
+		sortedInput(headPolySum, newElement);
+
+		currentPoly2 = currentPoly2->next;
+	}
+
+	return EXIT_SUCCESS;
+}
+
+int polyProduct(position firstElPoly1, position firstElPoly2, position headPolyProduct) {
+	position currentPoly1 = firstElPoly1, currentPoly2 = firstElPoly2,newElement=NULL;
+	int coeff = 0, exp = 0;
+
+	while (currentPoly1 != NULL) {
+
+		while (currentPoly2 != NULL) {
+	
+			coeff = currentPoly1->coeff * currentPoly2->coeff;
+			exp = currentPoly1->exp + currentPoly2->exp;
+
+			newElement=createElement(coeff, exp);
+			sortedInput(headPolyProduct, newElement);
+
+			currentPoly2 = currentPoly2->next;
+		
+		}
+
+		currentPoly1 = currentPoly1->next;
+
+		currentPoly2 = firstElPoly2;
+	}
+
+
+	return EXIT_SUCCESS;
+}
+
+int printPoly(char* polyName,position firstEl) {
+	position current = firstEl;
+
+	printf("\n----------------\n %s:  ",polyName);
+
+	while (current != NULL) {
+
+		if (current==firstEl) {
+
+			if(current->exp<0)
+				printf("%dx^(%d)", current->coeff, current->exp);
+		
+			else
+				printf("%dx^%d", current->coeff, current->exp);
+
+			current = current->next;
+		}
+
+		else {
+
+			if (current->exp < 0) {
+
+				if (current->coeff > 0)
+					printf("+%dx^(%d)", current->coeff, current->exp);
+
+				else
+					printf("-%dx^(%d)", abs(current->coeff), current->exp);
+			}
+
+			else {
+
+				if(current->coeff > 0)
+					printf("+%dx^%d", current->coeff, current->exp);
+
+				else
+					printf("-%dx^%d", abs(current->coeff), current->exp);
+			}
+
+			current = current->next;
+		}
+	}
+
+	printf("\n----------------\n");
+
+	return EXIT_SUCCESS;
+}
+
+
